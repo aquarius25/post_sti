7bf9b7cab560b35d2782e6399628c7fb258e186b
aqua25 <h.aqua25@gmail.com>
2019-05-24 11:08:08 +0900

    needless code delete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
   self.inheritance_column = :_type_disabled
-  mount_uploader :url, ImagesUploader
   has_one :content
   has_one :image
   accepts_nested_attributes_for :content
